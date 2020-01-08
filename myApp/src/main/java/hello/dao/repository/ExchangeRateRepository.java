7aae3ec7fd393502dd4c9be2612d80b50b9ae1d3
Pablo Atoche <pablo.atoche@sap.com>
2020-01-08 08:20:04 -0500

    agregando consulta

--- myApp/src/main/java/hello/dao/repository/ExchangeRateRepository.java
+++ myApp/src/main/java/hello/dao/repository/ExchangeRateRepository.java
@@ -2,6 +2,10 @@ package hello.dao.repository;
 
 import hello.dao.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+@Repository
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+
+    ExchangeRate findByCurrencyFromAndCurrencyTo(String currencyFrom, String currencyTo);
 }
