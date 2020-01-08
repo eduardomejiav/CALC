7fe5da4c00b6e75ff79aa039fc8fe43c16d96293
eduardomejiav <eduardof.mejiav@gmail.com>
2020-01-07 22:24:00 -0500

    agregando nueva aplicacion -  conversion de moneda

---
+++ myApp/src/main/java/hello/HelloController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+        public String index() {
+            return "This is the index of the site!\n";
+        }
+    @RequestMapping("/hello")
+        public String index2() {
+
+            return "Hello, cruel World!\n";
+        }
+    @RequestMapping("/bye")
+        public String index3() {
+
+            return "Bye, cruel World!\n";
+        }
+}
---
+++ myApp/src/main/java/hello/MainApplicationClass.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplicationClass {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(MainApplicationClass.class, args);
+//    }
+
+import java.util.Scanner;
+ 
+public class Exercise5 {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input first number: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Input second number: ");
+  int num2 = in.nextInt();
+   
+  System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+ }
+ 
+}
+
+}
